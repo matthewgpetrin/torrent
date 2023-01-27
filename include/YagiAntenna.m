05c85dc5509a4fc46a6e440c4dbc36e9f93b469e
matthewgpetrin <matthewgpetrin@gmail.com>
2023-01-26 21:28:27 -0500

    Restructured - .osm and .m function files now in /include/ and .m testing files in /test/

---
+++ include/YagiAntenna.m
@@ -0,0 +1,37 @@
+function antenna = YagiAntenna()
+
+% Variable definitions ----------------------------------------------------
+frequency = 2.45e9;
+
+exciterSpace = 25.4;
+exciterLength = 63.5;
+exciterDiameter = 25.4;
+
+directorCount = 18;
+
+directorLength = [38.1 38.1 38.1 38.1 38.1  ...
+                  38.1 38.1 38.1 38.1 38.1  ...
+                  38.1 38.1 38.1 38.1 38.1  ...
+                  38.1 38.1 38.1];
+directorSpace = [12.2 28.6 41.3 41.3 41.3  ...
+                 44.5 44.5 44.5 44.5 44.5  ...
+                 47.6 44.5 47.6 47.6 47.6  ...
+                 47.6 41.3 44.5];
+
+reflectorLength = 82.3;
+reflectorSpace = 15.9;
+
+% Create exciter element and antenna --------------------------------------
+exciter = design(dipoleFolded, frequency); % Auto generate dipole
+
+% We should create a dipole manually using real dimensions. I have not been
+% able to do so without getting errors
+
+antenna = yagiUda("Exciter", exciter, 'NumDirectors', 18, ...
+                  "DirectorLength", directorLength.*10^-3, ...
+                  "DirectorSpacing", directorSpace.*10^-3, ...
+                  "ReflectorLength", reflectorLength*10^-3, ...
+                  "ReflectorSpacing", reflectorSpace*10^-3);
+
+antenna.TiltAxis ='Z'; 
+antenna.Tilt = 90; % Aligns the antenna with the ground
