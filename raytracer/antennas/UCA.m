b8eac8bde34437c336cf3a7dfb7f9c1fdbaa88b1
matthewgpetrin <matthewgpetrin@gmail.com>
2023-05-09 16:25:08 -0400

    Refactored

--- raytracer/antennas/UCA.m
+++ raytracer/antennas/UCA.m
@@ -1,52 +1,52 @@
 function [array] = UCA(frequency, angle, nElements)
 
-% Create a uniform circular array using phased toolbox. This will be used
-% to calculate the necessary phase delays
-element = phased.IsotropicAntennaElement('FrequencyRange', [0 frequency]);
-
-phasedUCA = phased.UCA('NumElements', nElements, ...
-    'ArrayNormal', 'z', ...
-    'Taper', ones(size(nElements)), ...
-    'Radius' , 0.5.*(physconst('LightSpeed')/frequency), ...
-    'Element', element);
-
-if angle > 180
-    angle = -(360 - angle);
-end
-
-% Double check this
-steeringAngles = angle;
-phaseShiftBits = 6;
-
-% Find the weights
-w = zeros(getNumElements(phasedUCA), length(frequency));
-
-% Calculate steering vector of antenna
-steerVector = phased.SteeringVector('SensorArray', phasedUCA, ...
-    'PropagationSpeed', physconst('LightSpeed'), ...
-    'NumPhaseShifterBits', phaseShiftBits);
-
-for n = 1:length(frequency)
-    w(:, n) = step(steerVector, frequency(n), steeringAngles(:, n));
-end
-
-% Calculate phase shifts from phased toolbox array
-delay = phased.ElementDelay('SensorArray', phasedUCA);
-phaseShifts = delay(angle(1)).*360.*frequency;
-
-% Design array using antenna toolbox for accurate coverage
-antenna = design(dipole, 2.4e9);
-antenna.Length = 0.3937;
-antenna.Width = 0.01016;
-
-antennaUCA = circularArray("AmplitudeTaper", phasedUCA.Taper, ...
-    "Element", antenna, ...
-    "NumElements", phasedUCA.NumElements, ...
-    "PhaseShift", phaseShifts, ...
-    "Radius", phasedUCA.Radius, ...
-    "TiltAxis", [0 0 1], ...
-    "Tilt", 0);
-
-uca = design(antennaUCA, frequency, antenna);
-
-array = uca;
+    % Create a uniform circular array using phased toolbox. This will be used
+    % to calculate the necessary phase delays
+    element = phased.IsotropicAntennaElement('FrequencyRange', [0 frequency]);
+    
+    phasedUCA = phased.UCA('NumElements', nElements, ...
+        'ArrayNormal', 'z', ...
+        'Taper', ones(size(nElements)), ...
+        'Radius' , 0.5.*(physconst('LightSpeed')/frequency), ...
+        'Element', element);
+    
+    if angle > 180
+        angle = -(360 - angle);
+    end
+    
+    % Double check this
+    steeringAngles = angle;
+    phaseShiftBits = 6;
+    
+    % Find the weights
+    w = zeros(getNumElements(phasedUCA), length(frequency));
+    
+    % Calculate steering vector of antenna
+    steerVector = phased.SteeringVector('SensorArray', phasedUCA, ...
+        'PropagationSpeed', physconst('LightSpeed'), ...
+        'NumPhaseShifterBits', phaseShiftBits);
+    
+    for n = 1:length(frequency)
+        w(:, n) = step(steerVector, frequency(n), steeringAngles(:, n));
+    end
+    
+    % Calculate phase shifts from phased toolbox array
+    delay = phased.ElementDelay('SensorArray', phasedUCA);
+    phaseShifts = delay(angle(1)).*360.*frequency;
+    
+    % Design array using antenna toolbox for accurate coverage
+    antenna = design(dipole, 2.4e9);
+    antenna.Length = 0.3937;
+    antenna.Width = 0.01016;
+    
+    antennaUCA = circularArray("AmplitudeTaper", phasedUCA.Taper, ...
+        "Element", antenna, ...
+        "NumElements", phasedUCA.NumElements, ...
+        "PhaseShift", phaseShifts, ...
+        "Radius", phasedUCA.Radius, ...
+        "TiltAxis", [0 0 1], ...
+        "Tilt", 0);
+    
+    uca = design(antennaUCA, frequency, antenna);
+    
+    array = uca;
--- raytracer/antennas/YagiAntenna.m
+++ raytracer/antennas/YagiAntenna.m
@@ -2,13 +2,13 @@ function antenna = YagiAntenna()
 
 % Variable definitions ----------------------------------------------------
 frequency = 2.45e9;
-
+    
 exciterSpace = 25.4;
 exciterLength = 63.5;
 exciterDiameter = 25.4;
-
+    
 directorCount = 18;
-
+    
 % directorLength = [38.1 38.1 38.1 38.1 38.1  ...
 %                   38.1 38.1 38.1 38.1 38.1  ...
 %                   38.1 38.1 38.1 38.1 38.1  ...
@@ -17,27 +17,27 @@ directorCount = 18;
 %                  44.5 44.5 44.5 44.5 44.5  ...
 %                  47.6 44.5 47.6 47.6 47.6  ...
 %                  47.6 41.3 44.5];
-
+    
 directorSpace = [ 12, 27, 38.5, 37, 38.5, 40.5, 48.15, 43.5, 42.5, 44.8, 45.33, 46.9, 47, 49.1, 51, 49.85, 33.15, 38.1 ];
-
+    
 directorLength = [ 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 49.3 ];
-
+    
 %reflectorLength = 82.3;
 %reflectorSpace = 15.9;
 reflectorLength = 82.5;
 reflectorSpace = 13.7;
-
+    
 % Create exciter element and antenna --------------------------------------
 exciter = design(dipoleFolded, frequency); % Auto generate dipole
-
+    
 % We should create a dipole manually using real dimensions. I have not been
 % able to do so without getting errors
-
+    
 antenna = yagiUda("Exciter", exciter, 'NumDirectors', 18, ...
-                  "DirectorLength", directorLength.*10^-3, ...
-                  "DirectorSpacing", directorSpace.*10^-3, ...
-                  "ReflectorLength", reflectorLength*10^-3, ...
-                  "ReflectorSpacing", reflectorSpace*10^-3);
-
+                      "DirectorLength", directorLength.*10^-3, ...
+                      "DirectorSpacing", directorSpace.*10^-3, ...
+                      "ReflectorLength", reflectorLength*10^-3, ...
+                      "ReflectorSpacing", reflectorSpace*10^-3);
+    
 antenna.Tilt = 90;
 antenna.TiltAxis = [0 1 0];
