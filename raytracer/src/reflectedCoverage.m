563d0ffdd042ea9e268df98401f135dbabc5e062
matthewgpetrin <matthewgpetrin@gmail.com>
2023-05-09 16:28:04 -0400

    Refactored

--- raytracer/src/reflectedCoverage.m
+++ raytracer/src/reflectedCoverage.m
@@ -1,16 +1,14 @@
-function cov = bouncedCoverage(tx, options)
+function cov = reflectedCoverage(tx, options)
 
 arguments
     tx
 
-    options.Accuracy (1,1) string = "high"
-    options.Reflections (1,1) int16 = 1
+    options.inaccuracy (1,1) string = "high"
+    options.num_reflects (1,1) int16 = 1
 
-    options.TerrainMaterial (1,1) string = "perfect-reflector"
-    options.BuildingMaterial (1,1) string = "perfect-reflector"
+    options.terrain_material (1,1) string = "concrete"
+    options.building_material (1,1) string = "concrete"
 end
-
-addpath ../utils/;
 
 floor = noiseFloor(295, tx.TransmitterFrequency);
 
@@ -18,17 +16,17 @@ floor = noiseFloor(295, tx.TransmitterFrequency);
 
 prop_0 = propagationModel("raytracing", ...
     "Method", "sbr", ...
-    "AngularSeparation",options.Accuracy, ...
+    "AngularSeparation", options.inaccuracy, ...
     "MaxNumReflections", 0, ...
-    "TerrainMaterial", options.TerrainMaterial, ...
-    "BuildingsMaterial", options.BuildingMaterial);
+    "TerrainMaterial", options.terrain_material, ...
+    "BuildingsMaterial", options.building_material);
 
 prop_1 = propagationModel("raytracing", ...
     "Method", "sbr", ...
-    "AngularSeparation",options.Accuracy, ...
-    "MaxNumReflections", options.Reflections, ...
-    "TerrainMaterial", options.TerrainMaterial, ...
-    "BuildingsMaterial", options.BuildingMaterial);
+    "AngularSeparation",options.inaccuracy, ...
+    "MaxNumReflections", options.num_reflects, ...
+    "TerrainMaterial", options.terrain_material, ...
+    "BuildingsMaterial", options.building_material);
 
 %% Coverages macros --------------------------------------------------------
 coverage_0 = coverage(tx, prop_0, ...
@@ -50,19 +48,17 @@ table_0 = coverage_0.Data;
 table_1 = coverage_1.Data;
 table = table_1;
 
-table_0.Power = dBmToWatts(coverage_0.Data.Power);
-table_1.Power = dBmToWatts(coverage_1.Data.Power);
+table_0.Power = dBm2watts(coverage_0.Data.Power);
+table_1.Power = dBm2watts(coverage_1.Data.Power);
 
-table.Power = real(wattsTodBm(table_1.Power - table_0.Power));
-table_0.Power = real(wattsTodBm(coverage_0.Data.Power));
-table_1.Power = real(wattsTodBm(coverage_1.Data.Power));
+table.Power = real(watts2dBm(table_1.Power - table_0.Power));
+table_0.Power = real(watts2dBm(coverage_0.Data.Power));
+table_1.Power = real(watts2dBm(coverage_1.Data.Power));
 
 writetable(table_0,'../data/coverage_0.txt', 'WriteVariableNames', true);
-writetable(table_1,'../data/coverage_1.txt','WriteVariableNames', true);
-writetable(table, '../data/coverage','WriteVariableNames', true);
+writetable(table_1,'../data/coverage_1.txt', 'WriteVariableNames', true);
+writetable(table, '../data/coverage', 'WriteVariableNames', true);
 
 cov = propagationData(table);
-
-rmpath ../utils/;
 
 end
